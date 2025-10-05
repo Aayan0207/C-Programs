7b486f5deb0d6ebb9077413661c25fc0ef95243f
Aayan Khan <aayan.khan02072007@gmail.com>
2025-10-05 19:08:40 +0530

    4b

---
+++ Experiments-Sem_1/4b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+float fahrenheit(float temp) {
+    return (temp * 9 / 5) + 32;
+}
+
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    float temps[n];
+    for (int i = 0; i < n; i++) {
+        printf("Enter temperature %d: ", i + 1);
+        scanf("%f", &temps[i]);
+    }
+    printf("------------------------\n");
+    printf("| Celsius | Fahrenheit |\n");
+    printf("------------------------\n");
+    for (int i = 0; i < n; i++) {
+        printf("| %.2f  |\t%.2f |\n", temps[i], fahrenheit(temps[i]));
+    }
+    printf("------------------------\n");
+    return 0;
+}
