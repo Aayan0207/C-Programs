0b58822e90d4115dbddb3cacb3c5528b7ed06f15
Aayan Khan <aayan.khan02072007@gmail.com>
2025-11-20 21:50:04 +0530

    Experiment 9

--- Experiments-Sem_1/9g.c
+++ Experiments-Sem_1/9g.c
@@ -2,22 +2,36 @@
 
 //Counter of Elements in Array
 int main() {
-    int arr[100], counter[101], n, target;
+    int arr[100], n;
     printf("Enter n: ");
     scanf("%d", &n);
     printf("Enter numbers: \n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
+    }
+    int max = arr[0], min = arr[0];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    int range = max - min + 1;
+    int counter[range];
+    for (int i = 0; i <= range; i++) {
         counter[i] = 0;
     }
     for (int i = 0; i < n; i++) {
-        counter[arr[i]]++;
+        counter[arr[i] - min]++;
     }
     printf("Frequency of elements: \n");
     for (int i = 0; i < n; i++) {
-        if (counter[arr[i]] != 0) {
-            printf("%d occured %d times\n", arr[i], counter[arr[i]]);
-            counter[arr[i]] = 0;
+        int index = arr[i] - min;
+        if (counter[index] != 0) {
+            printf("%d occured %d times\n", arr[i], counter[index]);
+            counter[index] = 0;
         }
     }
     return 0;
